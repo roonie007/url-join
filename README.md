580afb885b8c742ea24f7f861c182fa5d51fd722
Mouadh HSOUMI <mouadh.hsoumi@pertimm.com>
2020-05-20 15:53:12 +0200

    [changed] prettier format

--- README.md
+++ README.md
@@ -7,9 +7,9 @@ Join all arguments together and normalize the resulting url.
 ## Usage
 
 ```javascript
-import { urlJoin } from "https://deno.land/x/url_join/join.ts";
+import { urlJoin } from 'https://deno.land/x/url_join/join.ts';
 
-const fullUrl = urlJoin("http://www.google.com", "a", "/b/cd", "?foo=123");
+const fullUrl = urlJoin('http://www.google.com', 'a', '/b/cd', '?foo=123');
 
 console.log(fullUrl);
 // http://www.google.com/a/b/cd?foo=123
--- join.ts
+++ join.ts
@@ -1,7 +1,7 @@
 export const urlJoin = function (...args: any[]) {
   let input;
 
-  if (typeof args[0] === "object") {
+  if (typeof args[0] === 'object') {
     input = args[0];
   } else {
     input = [].slice.call(args);
@@ -13,11 +13,11 @@ export const urlJoin = function (...args: any[]) {
 const normalize = (strArray: Array<string>) => {
   const resultArray = [];
   if (strArray.length === 0) {
-    return "";
+    return '';
   }
 
-  if (typeof strArray[0] !== "string") {
-    throw new TypeError("Url must be a string. Received " + strArray[0]);
+  if (typeof strArray[0] !== 'string') {
+    throw new TypeError('Url must be a string. Received ' + strArray[0]);
   }
 
   // If the first part is a plain protocol, we combine it with the next part.
@@ -28,46 +28,46 @@ const normalize = (strArray: Array<string>) => {
 
   // There must be two or three slashes in the file protocol, two slashes in anything else.
   if (strArray[0].match(/^file:\/\/\//)) {
-    strArray[0] = strArray[0].replace(/^([^/:]+):\/*/, "$1:///");
+    strArray[0] = strArray[0].replace(/^([^/:]+):\/*/, '$1:///');
   } else {
-    strArray[0] = strArray[0].replace(/^([^/:]+):\/*/, "$1://");
+    strArray[0] = strArray[0].replace(/^([^/:]+):\/*/, '$1://');
   }
 
   for (let i = 0; i < strArray.length; i++) {
     let component = strArray[i];
 
-    if (typeof component !== "string") {
-      throw new TypeError("Url must be a string. Received " + component);
+    if (typeof component !== 'string') {
+      throw new TypeError('Url must be a string. Received ' + component);
     }
 
-    if (component === "") {
+    if (component === '') {
       continue;
     }
 
     if (i > 0) {
       // Removing the starting slashes for each component but the first.
-      component = component.replace(/^[\/]+/, "");
+      component = component.replace(/^[\/]+/, '');
     }
     if (i < strArray.length - 1) {
       // Removing the ending slashes for each component but the last.
-      component = component.replace(/[\/]+$/, "");
+      component = component.replace(/[\/]+$/, '');
     } else {
       // For the last component we will combine multiple slashes to a single one.
-      component = component.replace(/[\/]+$/, "/");
+      component = component.replace(/[\/]+$/, '/');
     }
 
     resultArray.push(component);
   }
 
-  let str = resultArray.join("/");
+  let str = resultArray.join('/');
   // Each input component is now separated by a single slash except the possible first plain protocol part.
 
   // remove trailing slash before parameters or hash
-  str = str.replace(/\/(\?|&|#[^!])/g, "$1");
+  str = str.replace(/\/(\?|&|#[^!])/g, '$1');
 
   // replace ? in parameters with &
-  let parts = str.split("?");
-  str = parts.shift() + (parts.length > 0 ? "?" : "") + parts.join("&");
+  let parts = str.split('?');
+  str = parts.shift() + (parts.length > 0 ? '?' : '') + parts.join('&');
 
   return str;
 };
